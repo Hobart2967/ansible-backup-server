eee6c20792dc8756635cdb00c7cad1188125c821
Hobart2967 <hobart2967@outlook.com>
2026-02-06 22:36:21 +0100

    feat: Reconfigure backup server to use 1password and link everything to the new bare metal box

--- provision.sh
+++ provision.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 main () {
-  if [ "$#" -ne 3 ]; then
+  if [ "$#" -ne 2 ]; then
     echo "Illegal number of parameters. Usage: ./provision.sh <ip> <user> <keePassKdbxFile>"
     return
   fi
 
   echo "Provisioning machine '$1'"
   ssh-copy-id $2@$1
-  ansible-playbook -i "$1," provision-machine.yml --user $2 --diff --ask-become-pass --extra-vars "keepass_dbx=$3" --extra-vars "machine=$1"
+  ansible-playbook -i "$1," provision-machine.yml --user $2 --diff --ask-become-pass --extra-vars "machine=$1"
 }
 
-setup() {
-  pip install 'pykeepass==4.0.3' --user
-  ansible-galaxy collection install viczem.keepass
-}
 
 DIR=$(pwd)
 setup
