e43d736e7a790f7f37f9f2d9943ab020eaadb047
Hobart2967 <hobart2967@outlook.com>
2026-02-07 15:24:07 +0100

    feat: :sparkles: Finish backup implementation, add monitoring notifications and reporting

--- templates/usr/bin/mysql-backup.sh
+++ templates/usr/bin/mysql-backup.sh
@@ -11,8 +11,8 @@
 #RESTORE FROM BACKUP
   #$ gunzip < [backupfile.sql.gz] | mysql -u [uname] -p[pass] [dbname]
 
-if [ $# -ne 6 ]; then
-  echo "Usage: $0 <tunnel-name> <mysql-username> <mysql-password> <mysql-host> <mysql-port> <backup-directory>"
+if [ $# -ne 9 ]; then
+  echo "Usage: $0 <tunnel-name> <mysql-username> <mysql-password> <mysql-host> <mysql-port> <backup-directory> <log-directory> <job-name> <ntfy_token>"
   exit 1
 fi
 
@@ -29,6 +29,10 @@ MYSQL_PORT=$5
 # directory to put the backup files
 BACKUP_DIR=$6
 
+LOG_DIR=$7
+JOB_NAME=$8
+NTFY_TOKEN=$9
+
 # Don't backup databases with these names
 # Example: starts with mysql (^mysql) or ends with _schema (_schema$)
 IGNORE_DB="(^mysql|_schema$)"
@@ -44,13 +48,21 @@ KEEP_BACKUPS_FOR=30 #days
 #==============================================================================
 
 # YYYY-MM-DD
+
 TIMESTAMP=$(date +%F)
+output=""
+function log() {
+  echo $1
+  output+="$1\n"
+  echo "[$TIMESTAMP] $1" >> $LOG_DIR/mysql-${JOB_NAME}.txt
+}
 
 function delete_old_backups()
 {
-  echo "Deleting $BACKUP_DIR/*.sql.gz older than $KEEP_BACKUPS_FOR days"
-  find $BACKUP_DIR -type f -name "*.sql.gz" -mtime +$KEEP_BACKUPS_FOR -exec rm {} \;
-  echo "Done cleaning up."
+  log "Deleting $BACKUP_DIR/*.sql.gz older than $KEEP_BACKUPS_FOR days"
+  log $(find $BACKUP_DIR -type f -name "*.sql.gz" -mtime +$KEEP_BACKUPS_FOR -exec sh -c 'echo "{}"' \;)
+  find $BACKUP_DIR -type f -name "*.sql.gz" -mtime +$KEEP_BACKUPS_FOR -exec sh -c 'rm "{}"' \;
+  log "Done cleaning up."
 }
 
 function mysql_login() {
@@ -67,15 +79,15 @@ function database_list() {
 }
 
 function echo_status(){
-  printf '\r';
-  printf ' %0.s' {0..100}
-  printf '\r';
-  printf "$1"'\r'
+  log $(printf '\r');
+  log $(printf ' %0.s' {0..100})
+  log $(printf '\r';)
+  log $(printf "$1"'\r')
 }
 
 function backup_database(){
     backup_file="$BACKUP_DIR/$TIMESTAMP.$database.sql.gz"
-    output+="$database => $backup_file\n"
+    log "$database => $backup_file\n"
     echo_status "...backing up $count of $total databases: $database"
     $(mysqldump $(mysql_login) $database | gzip -9 > $backup_file)
 }
@@ -83,36 +95,45 @@ function backup_database(){
 function backup_databases(){
   local databases=$(database_list)
   local total=$(echo $databases | wc -w | xargs)
-  local output=""
   local count=1
   for database in $databases; do
     backup_database
     local count=$((count+1))
   done
-  echo -ne $output | column -t
 }
 
 function hr(){
-  printf '=%.0s' {1..100}
-  printf "\n"
+  log $(printf '=%.0s' {1..100})
+  log "\n"
 }
 
 #==============================================================================
 # RUN SCRIPT
 #==============================================================================
 
+curl -X POST \
+  -H "Authorization: Bearer $NTFY_TOKEN" \
+  -H "Title: Starting MySQL Backup $JOB_NAME" \
+  -d "Backup started at $(date)" \
+  https://ntfy.codewyre.net/backups
 
 mkdir -p $BACKUP_DIR
 
 delete_old_backups
 hr
-echo "Creating SSH tunnel for connections to ${MYSQL_HOST}:${MYSQL_PORT}..."
+log "Creating SSH tunnel for connections to ${MYSQL_HOST}:${MYSQL_PORT}..."
 /usr/sbin/service ssh-tunnel-$1 start
-echo "Established!"
+log "Established!"
 sleep 5
-echo "Initializing backup"
+log "Initializing backup"
 backup_databases
-echo "Done backing up, closing tunnel"
+log "Done backing up, closing tunnel"
 hr
 /usr/sbin/service ssh-tunnel-$1 stop
-printf "All backed up!\n\n"
+log "All backed up!"
+
+curl -X POST \
+  -H "Authorization: Bearer $NTFY_TOKEN" \
+  -H "Title: MySQL Backup $JOB_NAME completed" \
+  -d "$MESSAGE" \
+  https://ntfy.codewyre.net/backups
